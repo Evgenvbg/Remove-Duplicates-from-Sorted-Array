110b87aa905f66324ee2c6e257de480325b3a60d
Evgeniy Rudenya <104919093+Evgenvbg@users.noreply.github.com>
2023-01-15 15:38:11 +0300

    #Исправлена ошибка в итерации

--- Remove Duplicates from Sorted Array.cpp
+++ Remove Duplicates from Sorted Array.cpp
@@ -8,15 +8,17 @@ class Solution {
 public:
     int removeDuplicates(std::vector<int>& nums) {
         int insertNum = 1;
-        for (int i = 1; i < nums.size(); i++) {
+        for (int i = 1; i < nums.size(); i++) 
+        {
             if (nums[i - 1] != nums[i])
             {
                 nums[insertNum] = nums[i];
                 insertNum++;
             }
-
-            return insertNum;
+           
         }
+        return insertNum;
+
         for (int i = 0; i < nums.size(); i++) {
             std::cout << nums[i];
         }
@@ -25,7 +27,7 @@ public:
 
 int main()
 {
-    std::vector<int> vec{ 1,1,2,2,3,5,7 };
+    std::vector<int> vec{ 0,0,0,1,1,2,2,3,3,4 };
     Solution a;
     std::cout << a.removeDuplicates(vec);
 }
